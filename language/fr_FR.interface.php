0567d1c0e72df344a6c3311655465dd50f5f61ab
DESKTOP-E78O66O\cmaka <crismakabenta@gmail.com>
2017-10-09 13:03:48 +0800

    Melis Messenger: Added the install folder and french language file

---
+++ language/fr_FR.interface.php
@@ -0,0 +1,19 @@
+<?php
+    return array(
+        'tr_melismessenger_tool_name'                                   =>  'Messenger',
+        'tr_melismessenger_tool_input_placeholder'                      =>  'Ecrivez votre message..',
+        'tr_melistoolmesseger_tool_input_empty'                         =>  'Veuillez insérer votre message.',
+        'tr_melismessenger_tool_empty_conversation'                     =>  'Pas de conversation à afficher.',
+        'tr_melismessenger_tool_select_contact_todisplay_conversation'  =>  'Veuillez choisir un contact pour afficher la conversation.',
+        'tr_melismessenger_tool_create_button'                          =>  'Créer',
+        'tr_melismessenger_tool_conversation_with'                      =>  'Conversation avec',
+        'tr_melismessenger_tool_select_contact'                         =>  'Saisissez le nom du contact',
+        'tr_melismessenger_tool_no_more_msg_to_load'                    =>  'Plus de message à charger.',
+        'tr_melismessenger_tool_inbox_is_empty'                         =>  'Boîte de réception vide',
+        'tr_melismessenger_tool_new_message'                            =>  'Nouveau message à',
+        'tr_melismessenger_tool_inbox'                                  =>  'Boîte de réception',
+        'tr_melismessenger_tool_chat'                                   =>  'Chat',
+        'tr_melismessenger_tool_no_message_notification'                => 'Vous n\'avez pas de message',
+        'tr_melismessenger_tool_image_note'                             => 'Note: Toutes les images sont accessibles dans la bibliothèque multimédia, tout le monde peut les voir',
+        'tr_melismessenger_tool_user_is_deleted'                        => 'This user has been deleted.',
+    );
